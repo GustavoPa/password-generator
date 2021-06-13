f281e5919c44a1e0d38aefb01c29689eab39215b
GustavoPa <waleedtouqan@gmail.com>
2021-06-13 19:42:56 -0400

    generate pass func edited

--- script.js
+++ script.js
@@ -7,29 +7,18 @@ var confirmSpecial;
 var userChoices;
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Var To Upper Case 
+
+// turning lowercase letters to uppercase
 var blankUpper = [];
 var toUpper = function (x) {
     return x.toUpperCase();
 };
+// uppercase letters
 upperCase = lowerCase.map(toUpper);
+// numbers array
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//special chars array
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Generate password Function
 function generatePassword() {
@@ -140,3 +129,18 @@ function generatePassword() {
     return password;
 
 }
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
