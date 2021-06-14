e9ac92431a09e9248d709743d2f6496e80986922
GustavoPa <waleedtouqan@gmail.com>
2021-06-13 21:29:42 -0400

    passgen fun fixed

--- script.js
+++ script.js
@@ -7,14 +7,8 @@ var confirmSpecial;
 var userChoices;
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// turning lowercase letters to uppercase
-var blankUpper = [];
-var toUpper = function (x) {
-    return x.toUpperCase();
-};
 // uppercase letters
-upperCase = lowerCase.map(toUpper);
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 // numbers array
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 //special chars array
